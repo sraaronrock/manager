7d1c0806573162ff608752a1bd1cbd51bb300b44
Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>
2020-05-15 15:42:14 +0200

    feat(public-cloud): add sso auth modal
    ref: MANAGER-5262
    
    Signed-off-by: Cyrille Bourgois <cyrille.bourgois@corp.ovh.com>

--- packages/manager/apps/public-cloud/src/app.module.js
+++ packages/manager/apps/public-cloud/src/app.module.js
@@ -26,6 +26,7 @@ import ngOvhApiWrappers from '@ovh-ux/ng-ovh-api-wrappers';
 import ngOvhUserPref from '@ovh-ux/ng-ovh-user-pref';
 import ngUiRouterBreadcrumb from '@ovh-ux/ng-ui-router-breadcrumb';
 import ngUiRouterLineProgress from '@ovh-ux/ng-ui-router-line-progress';
+import ngOvhSsoAuthModalPlugin from '@ovh-ux/ng-ovh-sso-auth-modal-plugin';
 import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 
 import 'ovh-ui-kit/dist/oui.css';
@@ -58,6 +59,7 @@ angular
       ngUiRouterBreadcrumb,
       ngUiRouterLineProgress,
       ngOvhApiWrappers,
+      ngOvhSsoAuthModalPlugin,
       ngOvhUserPref,
       navbar,
       'oui',
