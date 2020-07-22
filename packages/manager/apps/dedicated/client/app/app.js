97b8fbdfb1201569a0b6608415921ed00addf843
Ganesh Kumar <ganesh.kumar@corp.ovh.com>
2020-07-22 13:04:05 +0530

    fix(dedicated): add account-migration notifications
    Fixes #MANAGER-5301
    
    Signed-off-by: Ganesh Kumar <ganesh.kumar@corp.ovh.com>

--- packages/manager/apps/dedicated/client/app/app.js
+++ packages/manager/apps/dedicated/client/app/app.js
@@ -46,6 +46,7 @@ import ovhManagerVrack from '@ovh-ux/manager-vrack';
 import ovhManagerIplb from '@ovh-ux/manager-iplb';
 import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 import ovhNotificationsSidebar from '@ovh-ux/manager-notifications-sidebar';
+import ovhManagerAccountMigration from '@ovh-ux/manager-account-migration';
 import account from './account';
 import config from './config/config';
 import contactsService from './account/contacts/service/contacts-service.module';
@@ -123,6 +124,7 @@ angular
       ngQAllSettled,
       'ovh-angular-responsive-tabs',
       'ovh-api-services',
+      ovhManagerAccountMigration,
       ovhManagerPccDashboard,
       ovhManagerIplb,
       ovhManagerPccResourceUpgrade,
