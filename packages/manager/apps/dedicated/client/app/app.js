9a135e390527ee79acd593fb20102604cabe1a0c
Ravindra Adireddy <ravindra.adireddy@ovhcloud.com>
2020-08-04 13:02:13 +0530

    feat(cloud-connect): ovh cloud connect
    develop cloud connect ui
    add manager core version
    fix validation issue
    add vrack success flow
    fix datagrid
    
    closes #MANAGER-5205
    
    BREAKING CHANGE: init `@ovh-ux/manager-cloud-connect` module.
    
    Signed-off-by: varun257 <varun257@gmail.com>

--- packages/manager/apps/dedicated/client/app/app.js
+++ packages/manager/apps/dedicated/client/app/app.js
@@ -43,6 +43,7 @@ import ovhManagerVeeamCloudConnect from '@ovh-ux/manager-veeam-cloud-connect';
 import ovhManagerVps from '@ovh-ux/manager-vps';
 import ovhManagerVrack from '@ovh-ux/manager-vrack';
 import ovhManagerIplb from '@ovh-ux/manager-iplb';
+import ovhManagerCloudConnect from '@ovh-ux/manager-cloud-connect';
 import { detach as detachPreloader } from '@ovh-ux/manager-preloader';
 import ovhNotificationsSidebar from '@ovh-ux/manager-notifications-sidebar';
 import account from './account';
@@ -133,6 +134,7 @@ angular
       ovhManagerNavbar,
       ovhManagerVps,
       ovhManagerVrack,
+      ovhManagerCloudConnect,
       ovhPaymentMethod,
       'pascalprecht.translate',
       'services',
@@ -234,6 +236,7 @@ angular
       });
 
       $state.defaultErrorHandler((error) => {
+        console.log(error);
         if (error.type === RejectType.ERROR) {
           $rootScope.$emit('ovh::sidebar::hide');
           $state.go(
