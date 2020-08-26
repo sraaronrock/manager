ffb63f8ae0cbac9e3fe13545477c28b6adf1372c
Marie JONES <marie.jones@corp.ovh.com>
2020-08-26 09:12:02 +0200

    feat: create sidebar fragment
    BREAKING CHANGE: init sidebar fragment module
    
    Signed-off-by: Marie JONES <marie.jones@corp.ovh.com>

---
+++ packages/manager/fragments/sidebar/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <nav role="navigation" class="oui-sidebar">
+    <div class="oui-sidebar-menu">
+      <Order :baseUrl="baseUrl" :universe="universe"></Order>
+      <Items :items="config.items"></Items>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Items from './components/items/Items.vue';
+import Order from './components/order/Order.vue';
+import { Sidebar } from './model/Sidebar';
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    Items,
+    Order,
+  },
+  props: {
+    universe: String,
+    items: Array,
+    config: Sidebar,
+    baseUrl: String,
+  },
+});
+</script>
---
+++ packages/manager/fragments/sidebar/src/components/order/Order.vue
@@ -0,0 +1,20 @@
+<template>
+  <a class="oui-button oui-button_primary" :href="discoverUrl">{{ $t('sidebar_discover') }}</a>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'Order',
+  props: {
+    baseUrl: String,
+    universe: String,
+  },
+  computed: {
+    discoverUrl() {
+      return `${this.baseUrl}/#/catalog?universe=${this.universe}`;
+    },
+  },
+});
+</script>
